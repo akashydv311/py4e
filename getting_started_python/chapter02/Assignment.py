e2a68091fcf491c47b3ede6150f9b2ef209d4892
Akash Yadav <akashydv311@gmail.com>
2025-07-29 04:26:06 +0530

    added chapter 02 assignment

--- getting_started_python/chapter02/Assignment.py
+++ getting_started_python/chapter02/Assignment.py
@@ -1,5 +1,6 @@
 
-# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will pop up a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them.
+# Note that input will pop up a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
 
 
 # The code below almost works
@@ -8,7 +9,9 @@ print("Hello "+name)
 
 
 
-# 2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+# 2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
+# Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. 
+# Do not worry about error checking or bad user data.
 
 # This first line is provided for you
 
