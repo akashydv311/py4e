c5df2bb666a845000e6ac62821e8d89470150964
Akash Yadav <akashydv311@gmail.com>
2025-08-23 08:20:24 +0530

    added chapter08 assignment

---
+++ python_data_structure/chapter08/test_work.py
@@ -0,0 +1,35 @@
+
+#l = []
+#print(type(l))
+#
+#print(range(10))
+#print(type(range(10)))
+
+
+
+#l1 = [32,32,23]
+#print(id(l1))
+#l1.append(332)
+#print(id(l1))
+
+
+#fruit = 'Banana'
+#fruit[0] = 'b'
+#print(fruit)
+
+data = '''But soft what light through yonder window breaks
+It is the east and Juliet is the sun
+Arise fair sun and kill the envious moon
+Who is already sick and pale with grief'''
+
+
+print(data.split())
+
+
+
+
+
+
+
+
+
